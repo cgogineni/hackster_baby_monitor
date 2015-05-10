1191b2801a8c8529bfe8dfc925771e928f126003
mathgeek404@mgmail.com <mathgeek404@gmail.com>
2015-05-10 15:20:20 -0400

    GETTER

--- www/js/main.js
+++ www/js/main.js
@@ -122,7 +122,7 @@ function validateIP() {
     var socket,
     //Get values from text fields
         //ip_addr = $("#ip_address").val(),
-        ip_addr = "10.3.1.83",
+        ip_addr = "192.168.43.23",
         //port = $("#port").val(),
         port = "8080",
         
@@ -137,45 +137,32 @@ function validateIP() {
         console.log("before try");
         try {
             // MODIFY THIS TO THE APPROPRIATE URL IF IT IS NOT BEING RUN LOCALLY
-            
+     
+           var socket = io.connect('http://192.168.43.23:8080');
+           console.log("after io.connect");       
+           window.location.assign("#select_box");
 
-            socket.on('frame', function (data) {
-				if(!isStream) {
-				   wantStream();	
-				   
-				   var socket = io.connect('http://10.3.1.83:8080');
-                   console.log("after io.connect");
-
-                   var content = document.getElementById('container');
-                   content.innerHTML = "";
-            
-                   var canvas = document.createElement('canvas');
-                   content.appendChild(canvas);
-                   var img = document.createElement("img");
-
-                   var context = canvas.getContext('2d');
-            //var img = new Image();
-
-            // show loading notice
-                  context.fillStyle = '#333';
-                  context.fillText('Loading...', canvas.width/2-30, canvas.height/3);
-				}
+           socket.on('frame', function (data) {
+				if(isStream ) {
+					canvas = document.getElementById('stream_canvas');
+				    var img = document.createElement("img");
 				
                 // Reference: http://stackoverflow.com/questions/24107378/socket-io-began-to-s
-                var uint8Arr = new Uint8Array(data.buffer);
-                var str = String.fromCharCode.apply(null, uint8Arr);
-                var base64String = btoa(str);
-                console.log("inside socket.on");
-                img.onload = function () {
-                   context.drawImage(this, 0, 0, canvas.width, canvas.height);
-                  };
-                img.src = 'data:image/png;base64,' + base64String;
+                    var uint8Arr = new Uint8Array(data.buffer);
+                    var str = String.fromCharCode.apply(null, uint8Arr);
+                    var base64String = btoa(str);
+                    console.log("inside socket.on");
+                    img.onload = function () {
+                       context.drawImage(this, 0, 0, canvas.width, canvas.height);
+                    };
+                    img.src = 'data:image/png;base64,' + base64String;
+			   }
             });
 
 			socket.on('alert',function(options) {
 				if (!alertBox) {
 				   console.log("ALERT");
-				   window.location="#alert_box"; 
+				   window.location.assign("#alert_box"); 
 				}
 					
 				
@@ -202,9 +189,22 @@ function validateIP() {
 }
 
 function startStream() {
-	isStream = true;
 	
 	var defaultPage = document.getElementById('default_container');
     defaultPage.style.display = "none";
+    
+    var videoPage = document.getElementById('video_container');
+    videoPage.style.display = "block";
+    
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute("id", "stream_canvas");
+    videoPage.appendChild(canvas);
+
+    var context = canvas.getContext('2d');
+
+    context.fillStyle = '#333';
+    context.fillText('Loading...', canvas.width/2-30, canvas.height/3);
+    
+    isStream = true;
 }
 
